3dc297b19464ed35ff9068eacabec241890ca01d
itning <itning@itning.top>
2019-06-25 11:17:21 +0800

    fix bug

--- src/main/java/top/itning/tmpp/tmppadmin/service/impl/AdminUserRoleServiceImpl.java
+++ src/main/java/top/itning/tmpp/tmppadmin/service/impl/AdminUserRoleServiceImpl.java
@@ -6,6 +6,7 @@ import top.itning.tmpp.tmppadmin.mapper.AdminUserRoleMapper;
 import top.itning.tmpp.tmppadmin.pojo.AdminUserRole;
 import top.itning.tmpp.tmppadmin.service.AdminUserRoleService;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +21,19 @@ public class AdminUserRoleServiceImpl implements AdminUserRoleService {
     @Autowired
     public AdminUserRoleServiceImpl(AdminUserRoleMapper adminUserRoleMapper) {
         this.adminUserRoleMapper = adminUserRoleMapper;
+        Date date = new Date();
+        try {
+            adminUserRoleMapper.insert(new AdminUserRole(1, "管理员", date, date, "ADMIN"));
+        } catch (Exception ignore) {
+        }
+        try {
+            adminUserRoleMapper.insert(new AdminUserRole(2, "教务处", date, date, "A"));
+        } catch (Exception ignore) {
+        }
+        try {
+            adminUserRoleMapper.insert(new AdminUserRole(3, "办公室主任", date, date, "O"));
+        } catch (Exception ignore) {
+        }
     }
 
     @Override
